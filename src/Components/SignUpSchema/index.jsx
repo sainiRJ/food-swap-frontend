995dce8e696b32eeecabeac66969d5983bb55688
Harshit <harshdh55@gmail.com>
2024-04-16 12:22:42 +0530

    added login page

--- src/Components/SignUpSchema/index.jsx
+++ src/Components/SignUpSchema/index.jsx
@@ -4,8 +4,8 @@ export const signUpSchema=Yup.object({
     password:Yup.string().max(15).required("Please enter your password"),
 })
 export const signInSchema=Yup.object({
-    email:Yup.string().min(6).required("Please enter your email"),
-    password:Yup.string().min(6).required("Please enter your password"),
+    email:Yup.string().required("Please enter your email"),
+    password:Yup.string().min(6).required("Your password length must be 6 words"),
 })
 export const emailModalSchema=Yup.object({
     email:Yup.string().min(6).required("Please enter your email")
